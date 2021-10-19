5bba9e317ddb35bb2d897b5f18e65e3d8024b38e
Ksenx73 <hello@itcampe.com>
2021-10-19 12:08:30 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,11 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("числа равны");
+        //напишите тут ваш код
+        System.out.println("числа не равны");
+    }
+}
